83162a38ddb6dd424a1e62ac4f4469ab709e65be
Aryan Karamtoth <88923986+SpaciousCoder78@users.noreply.github.com>
2024-05-22 07:44:24 +0530

    added more changes to login

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,15 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import login from './components/login.vue';
 </script>
 
 <template>
   <div>
-    <h1 id="banner">The Ho Chi Minh Trail</h1>
-    <form >
-      <h2>Login</h2>
-      <label>User ID:  </label>
-      <input>
-      <br><br>
-      <label>Password: </label>
-      <input>
-      <br><br>
-      <button type="button">Connect</button>
-    </form>
-    
+    <login/> 
   </div>
 </template>
 
 <style scoped>
-#banner{
-  padding:10%;
-  font-family:Dancing Script;
-}
 .logo {
   height: 6em;
   padding: 1.5em;
---
+++ src/components/login.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+    <h1 id="banner">The Ho Chi Minh Trail</h1>
+    <div class="container my-5">
+      <div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg">
+    <form >
+      <h2 id="loginbanner">Login</h2>
+      <label>User ID:  </label>
+      <input>
+      <br><br>
+      <label>Password: </label>
+      <input>
+      <br><br>
+      <button type="submit" class="btn btn-primary" >Connect</button>
+      <button type="button" class="btn btn-primary">Setup</button>
+      <br><br>
+    </form>
+      </div>
+    </div>
+    
+  </div>
+</template>
+
+<style>
+
+button{
+  margin-right:16px
+}
+#loginbanner{
+  padding: 10%;
+}
+#banner{
+  padding:5%;
+  font-family:Dancing Script;
+}
+</style>

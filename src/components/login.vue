6ad928c78d33163003c79e867c368ed6df0fc3d0
Aryan Karamtoth <88923986+SpaciousCoder78@users.noreply.github.com>
2024-05-22 07:51:45 +0530

    added comments to make it more readable

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,19 +1,25 @@
 <template>
     <div>
     <h1 id="banner">The Ho Chi Minh Trail</h1>
+    <!--Login container-->
     <div class="container my-5">
       <div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg">
     <form >
+      <!--login banner-->
       <h2 id="loginbanner">Login</h2>
+      <!--User id field-->
       <div class="input-group mb-3">
         <span class="input-group-text" id="basic-addon1">@</span>
         <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
       </div>
+      <!--Password field-->
       <div class="input-group mb-3">
         <input type="password" class="form-control" placeholder="Password" aria-label="Password" aria-describedby="basic-addon1">
       </div>
       <br><br>
+      <!--Connect button-->
       <button type="submit" class="btn btn-primary" >Connect</button>
+      <!--Setup button-->
       <button type="button" class="btn btn-primary">Setup</button>
       <br><br>
     </form>

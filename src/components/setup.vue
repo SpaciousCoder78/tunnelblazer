c6ce2a732baf5d4c80d8121eea55b747d95d604b
Aryan Karamtoth <88923986+SpaciousCoder78@users.noreply.github.com>
2024-05-23 17:36:23 +0530

    working on data binding

--- src/components/setup.vue
+++ src/components/setup.vue
@@ -11,7 +11,7 @@
                     <path d="M0 11.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5zm4-3a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5v5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5zm4-3a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5zm4-3a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5v11a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5z"/>
                     </svg>
                 </span>
-                <input type="text" class="form-control" placeholder="Host Address" aria-label="Host Address" aria-describedby="basic-addon1">
+                <input type="text" class="form-control" v-model="ip" placeholder="Host Address" aria-label="Host Address" aria-describedby="basic-addon1">
             </div>
             <!--Port-->
             <div class="input-group mb-3">
@@ -20,7 +20,7 @@
                     <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM1 2a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1z"/>
                     </svg>
                 </span>
-                <input type="text" class="form-control" placeholder="Port" aria-label="Port" aria-describedby="basic-addon1">
+                <input type="text" class="form-control" v-model="port" placeholder="Port" aria-label="Port" aria-describedby="basic-addon1">
             </div>
             <!--Time out-->
             <div class="input-group mb-3">
@@ -29,7 +29,7 @@
                     <path d="M6.5 1A.5.5 0 0 1 7 .5h2a.5.5 0 0 1 0 1v.57c1.36.196 2.594.78 3.584 1.64l.012-.013.354-.354-.354-.353a.5.5 0 0 1 .707-.708l1.414 1.415a.5.5 0 1 1-.707.707l-.353-.354-.354.354-.013.012A7 7 0 1 1 7 2.071V1.5a.5.5 0 0 1-.5-.5M8 3a6 6 0 1 0 .001 12A6 6 0 0 0 8 3"/>
                     </svg>
                 </span>
-                <input type="text" class="form-control" placeholder="Timeout (Optional)" aria-label="Timeout(Optional)" aria-describedby="basic-addon1">
+                <input type="text" class="form-control" v-model="timeout" placeholder="Timeout (Optional)" aria-label="Timeout(Optional)" aria-describedby="basic-addon1">
             </div>
             <!--Log File Path-->
             <div class="input-group mb-3">
@@ -37,18 +37,60 @@
                     <path fill-rule="evenodd" d="M14 4.5V14a2 2 0 0 1-2 2h-2v-1h2a1 1 0 0 0 1-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5zM1.928 15.849v-3.337h1.136v-.662H0v.662h1.134v3.337zm4.689-3.999h-.894L4.9 13.289h-.035l-.832-1.439h-.932l1.228 1.983-1.24 2.016h.862l.853-1.415h.035l.85 1.415h.907l-1.253-1.992zm1.93.662v3.337h-.794v-3.337H6.619v-.662h3.064v.662H8.546Z"/>
                 </svg>
             </span>
-                <input type="text" class="form-control" placeholder="Log File Path (Optional)" aria-label="Log File Path" aria-describedby="basic-addon1">
+                <input type="text" class="form-control" v-model="logpath" placeholder="Log File Path (Optional)" aria-label="Log File Path" aria-describedby="basic-addon1">
             </div>
             <br>
 
             <!--Save Configuration button-->
-            <button type="submit" class="btn btn-dark" >Save Configuration</button>
+            <button type="submit" class="btn btn-dark" @click.prevent="saveData" >Save Configuration</button>
             <br><br>
 
         </form>
     </div>
 </template>
 
+<!-------------------------------------------JavaScript Area------------------------------>
+<script>
+    import { ref } from 'vue';
+    import fs from 'fs';
+
+export default {
+  setup() {
+    const ip = ref('');
+    const port = ref('');
+    const timeout = ref('');
+    const logpath = ref('');
+
+    const saveData = () => {
+      const data = {
+        ip:ip.value,
+        port: port.value,
+        timeout: timeout.value,
+        logpath: logpath.value
+      };
+      require('fs').writeFileSync('data.json', JSON.stringify(data));
+
+      /*
+
+      ######################Code for client side###################
+      
+      const dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(data));
+      const downloadAnchorNode = document.createElement('a');
+      downloadAnchorNode.setAttribute("href",     dataStr);
+      downloadAnchorNode.setAttribute("download", "data.json");
+      document.body.appendChild(downloadAnchorNode); // required for firefox
+      downloadAnchorNode.click();
+      downloadAnchorNode.remove();*/
+    };
+
+    return { ip, port, timeout, logpath, saveData };
+  }
+};
+
+</script>
+
+
+<!---------------------------------------------CSS Area------------------------------------->
 <style>
 #setupbanner{
     padding: 10%;

353e716851692075d708679fc4bcc209a39e3775
Aryan Karamtoth <88923986+SpaciousCoder78@users.noreply.github.com>
2024-06-29 13:54:45 +0530

    disabled maximise button

--- src/main.js
+++ src/main.js
@@ -19,6 +19,7 @@ const createWindow = () => {
     height: 600,
     autoHideMenuBar:true,
     center:true,
+    maximizable:false,
     icon: 'https://media.discordapp.net/attachments/981123797392424971/1256518578660835369/Tunnelblazer.png?ex=66810f9e&is=667fbe1e&hm=282f86f89de7bc43bda92bbb40d8bcbffe872f8bbb4e451aae142d9501b01e5e&=&format=webp&quality=lossless&width=525&height=525',
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
